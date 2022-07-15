dce0ba45d8e062535e75f722f883dc7855181a9b
Dup4 <lyuzhi.pan@gmail.com>
2022-07-15 10:45:47 +0800

    chore: rename index filename

--- test/hello_world_benchmark.cc
+++ test/hello_world_benchmark.cc
@@ -1,10 +1,7 @@
 #include <benchmark/benchmark.h>
 
-#include "better-enum/hello_world.h"
-
 static void BenchmarkHelloWorld(benchmark::State& state) {
     for (auto _ : state) {
-        HelloWorld();
     }
 }
 
