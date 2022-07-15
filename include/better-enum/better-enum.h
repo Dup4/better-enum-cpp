a085fe359597ee0e6a7edee6b97bf03fc6f8b88b
Dup4 <lyuzhi.pan@gmail.com>
2022-07-15 10:46:27 +0800

    feat: main class

--- include/better-enum/better-enum.h
+++ include/better-enum/better-enum.h
@@ -1,4 +1,12 @@
 #ifndef BETTER_ENUM_BETTER_ENUM_H
 #define BETTER_ENUM_BETTER_ENUM_H
 
+namespace better_enum {
+
+class BetterEnum {
+public:
+};
+
+}  // namespace better_enum
+
 #endif  // BETTER_ENUM_BETTER_ENUM_H
